61c3ae432f4b402f0fda30b55c45dcefdcf43b6f
yossefsabry <yossefsabry@gmail.com>
2024-01-02 21:56:41 +0200

    'save'

--- venv/include/main.py
+++ venv/include/main.py
@@ -2,8 +2,6 @@ import pygame
 from pygame.locals import *
 import os
 
-from pygame.time import Clock
-
 # initailzation for pygame :: like setup all the things in pygame
 pygame.init()
 
@@ -11,7 +9,7 @@ pygame.init()
 clock = pygame.time.Clock()
 fbs = 60
 
-# setup for the window 
+# setup for the window
 SCREEN_WIDTH = 864
 SCREEN_HEIGHT = 936
 
@@ -25,18 +23,32 @@ flying = False
 game_over = False
 
 # load images for the game
-current_dir = os.getcwd() 
+current_dir = os.getcwd()
 img_folder = "/venv/include/img/"
 bg = pygame.image.load(f'{current_dir}{img_folder}bg.png')
 ground_image = pygame.image.load(f'{current_dir}{img_folder}ground.png')
 
+
+# start bird class
+class Bird(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.x = x
+        self.y = y
+
+
 run = True
 while run:
     clock.tick(fbs)
-    
+
     # display the images for the background
-    screen.blit(bg, (0,0))
-    screen.blit(ground_image ,(ground_scroll, 768))
+    screen.blit(bg, (0, 0))
+    screen.blit(ground_image, (ground_scroll, 768))
+
+    # start scroll the ground
+    ground_scroll -= scroll_speed
+    if abs(ground_scroll) > 35:
+        ground_scroll = 0
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -47,7 +59,3 @@ while run:
 
     pygame.display.update()
 pygame.quit()
-
-
-
-
